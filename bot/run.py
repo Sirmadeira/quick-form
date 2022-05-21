127c1c63e88cb0387dfb87cd5187e60ae54d1ce1
Yappy <francisco.froes@globalhitss.com.br>
2022-05-21 19:58:07 -0300

    Formanando

--- bot/run.py
+++ bot/run.py
@@ -2,5 +2,5 @@ from navigator.form import Driver
 
 #Aviso: O exite method e executado
 #Ao sair do with statement
-with Driver(switch = True) as bot:
+with Driver(switch = False) as bot:
     bot.landing_page()
